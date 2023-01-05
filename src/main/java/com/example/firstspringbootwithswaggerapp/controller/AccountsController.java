cb976bd44d0ce0289ceb69f73190aef04ff859bb
gopikrishnareddy200@gmail.com <gopikrishnareddy198@gmail.com>
2023-01-05 14:17:40 -0800

    Added Swagger Api Annotations to the controller class and their methods

--- src/main/java/com/example/firstspringbootwithswaggerapp/controller/AccountsController.java
+++ src/main/java/com/example/firstspringbootwithswaggerapp/controller/AccountsController.java
@@ -1,19 +1,24 @@
 package com.example.firstspringbootwithswaggerapp.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/accounts")
+@Api(value = "AccountsInformation")
 public class AccountsController {
 
 
     @RequestMapping(method = RequestMethod.GET,path = "/bill/{id}/{name}")
-    public String getBills(){
+    @ApiOperation(value = "Fetches the bill details for the given id and name",response = String.class)
+     public String getBills(@PathVariable("id")String id,@PathVariable("name")String name){
 
-        return "Hello Swagger";
+        return "Hey "+name+" your bill for the id: "+id+" is "+0.0;
     }
 
 
