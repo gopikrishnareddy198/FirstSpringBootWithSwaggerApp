6e1c5b3b0b3919e28c176ad69dd5c5832ec2de9e
gopikrishnareddy200@gmail.com <gopikrishnareddy198@gmail.com>
2023-01-05 13:58:55 -0800

    Added Swagger Configuration File

---
+++ src/main/java/com/example/firstspringbootwithswaggerapp/configuration/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.firstspringbootwithswaggerapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.RequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfiguration {
+
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+}
